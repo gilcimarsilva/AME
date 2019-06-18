7c4aa5529587870785b624f1cfb72979e6c11c71
gilcimar.silva <gilcimar.silva@to-brasil.com>
2019-06-17 21:32:34 -0300

    Commit Inicial

---
+++ WebApplicationContato/DAL/PlanetaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using WebApplicationContato.Models;
+
+
+namespace WebApplicationContato.DAL
+{
+    public class PlanetaDbContext : DbContext
+    {
+        public DbSet<Planeta> planetas { get; set; }        
+
+    }
+
+}
